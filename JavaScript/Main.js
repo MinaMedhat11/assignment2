bd8c5fc4a43d8049f3e483514ae695e315a58850
MinaMedhat11 <153822301+MinaMedhat11@users.noreply.github.com>
2023-12-14 13:18:42 +0200

    Main.js

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,4 +1,4 @@
-var qoutes = [
+var quotes = [
     {
         Quote: "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”",
         author: "― Martin Luther King"
